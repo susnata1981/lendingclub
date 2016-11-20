6a219743d8e55c4237a9f90892d14e509e9364ea
susnata basak <susnata@gmail.com>
2016-11-20 12:03:32 -0800

    Updated notification structure.

--- shared/bli/viewmodel/notification.py
+++ shared/bli/viewmodel/notification.py
@@ -4,13 +4,21 @@ class Notification:
     # NOTIFICATION_TYPE
     SUCCESS, WARNING, NORMAL, ERROR = range(4)
 
-    def __init__(self, title, clazz, notification_type, description=None, url1=None, url2=None):
+    def __init__(self, title, notification_type, description=None, url1=None, url2=None):
         self.title = title
         self.description = description
-        self.clazz = clazz
         self.notification_type = notification_type
+        self.clazz = self.get_css_class()
         self.url1 = url1
         self.url2 = url2
+
+    def get_css_class(self):
+        if self.notification_type == Notification.SUCCESS or self.notification_type == Notification.NORMAL:
+            return 'alert-success'
+        elif self.notification_type == Notification.WARNING:
+            return 'alert-warning'
+        elif self.notification_type == Notification.ERROR:
+            return 'alert-danger'
 
     def to_map(self):
         return self.__dict__

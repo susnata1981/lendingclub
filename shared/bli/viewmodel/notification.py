4962f6be6d42e441fe3f71161ee82969cbf60532
susnata basak <susnata@gmail.com>
2016-11-20 11:38:38 -0800

    Updated dashboard + loan_details page.

--- shared/bli/viewmodel/notification.py
+++ shared/bli/viewmodel/notification.py
@@ -4,9 +4,10 @@ class Notification:
     # NOTIFICATION_TYPE
     SUCCESS, WARNING, NORMAL, ERROR = range(4)
 
-    def __init__(self, title,  notification_type, description=None, url1=None, url2=None):
+    def __init__(self, title, clazz, notification_type, description=None, url1=None, url2=None):
         self.title = title
         self.description = description
+        self.clazz = clazz
         self.notification_type = notification_type
         self.url1 = url1
         self.url2 = url2
